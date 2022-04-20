6c20cce5b35168a2777f55b8ed5b4ad9f8196726
Patrick Edwards <pat.e1@icloud.com>
2022-04-21 01:52:03 +1000

    updated responsivness and styling

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ function App() {
   return (
     <div className="App">
       <div className="relative bg-[#1C1E23]">
-        <div className="origin-top-left absolute left-12 top-8 scale-[0.2]">
-          <Logo/>
+        <div className="origin-top-left absolute left-12 flex-1 top-8 xl:scale-[0.2] scale-0">
+          <Logo />
         </div>
         <MenuBar />
         <div className="relative flex flex-col xl:flex-row">
-          <div className="relative flex flex-col gap-7 flex-1 items-center justify-center">
+          <div className="relative flex flex-col md:gap-7 flex-1 items-center justify-center">
             <div className="scale-50 sm:scale-100 m-0 xl:m-0">
               <Logo />
             </div>
-            <FindPathButton />
+            <div>
+              <FindPathButton />
+            </div>
           </div>
           <div className="relative flex flex-1 items-center justify-center">
             <div className="scale-50 xl:scale-100">
@@ -28,6 +30,9 @@ function App() {
             </div>
           </div>
         </div>
+        <p>
+          
+        </p>
       </div>
     </div>
   );
--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,13 +1,13 @@
 function MenuBar() {
     return ( 
         <div className="relative flex flex-row justify-evenly w-full">
-            <div className="text-center font-lexendZetta text-white my-14 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
+            <div className="text-center font-lexendZetta text-white md:my-14 m-6 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
                 home
             </div>
-            <div className="text-center font-lexendZetta text-white my-14 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
+            <div className="text-center font-lexendZetta text-white md:my-14 m-6 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
                 six degrees
             </div>
-            <div className="text-center font-lexendZetta text-white my-14 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
+            <div className="text-center font-lexendZetta text-white md:my-14 m-6 text-xl scale-y-75 tracking-widest hover:line-through hover:cursor-pointer">
                 about
             </div>
         </div>
