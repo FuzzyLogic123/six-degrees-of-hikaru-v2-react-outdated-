68bf20bd2d0fab063daf829feb21731220d6dacc
FuzzyLogic123 <pat.e1@icloud.com>
2022-07-24 21:43:53 +1000

    added quotes to testimonials

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { ReactComponent as Quote } from "../../svg/quote.svg";
 
 function Testimonial({ quote }) {
     return (
-    <div>
-        <div className={`relative bg-[#31415985] leading-8 p-4 pt-24 text-center text-white text-xl rounded-lg sm:max-w-[10rem] lg:max-w-md 2xl:max-w-2xl mb-32 sm:mb-0`}>
-            <img src={quote[2]} alt="The Pope" className="absolute w-40 h-40 left-0 right-0 m-auto -top-24 rounded-full object-cover" />
-            <p className="font-Outfit 2xl:text-2xl sm:text-sm">
-                {quote[0]}
-            </p>
-            <br />
-            <p className="sm:text-4xl md:text-2xl font-semibold font-Ephesis tracking-[5px]">
-                {"-" + quote[1]}
-            </p>
-        </div>
-    </div> );
+        <div>
+            <div className={`relative bg-[#31415985] leading-8 p-4 pt-24 text-center text-white text-xl rounded-lg sm:max-w-[13rem] lg:max-w-md 2xl:max-w-2xl mb-32 sm:mb-0 lg:m-6`}>
+                <Quote className='stroke-white stroke-2 fill-transparent opacity-90 absolute -left-8 -top-4 h-14 z-10' />
+                <Quote className='stroke-white stroke-2 fill-transparent opacity-90 absolute -right-16 top-44 h-14 z-10 -scale-100 rotate-2' />
+                <img src={quote[2]} alt="The Pope" className="absolute z-20 w-40 h-40 left-0 right-0 m-auto -top-24 rounded-full object-cover" />
+                <p className="font-Outfit 2xl:text-2xl sm:text-sm md:text-lg">
+                    {quote[0]}
+                </p>
+                <br />
+                <p className="sm:text-4xl md:text-4xl font-semibold font-Ephesis tracking-[5px]">
+                    {"-" + quote[1]}
+                </p>
+            </div>
+        </div>);
 }
 
 export default Testimonial;
