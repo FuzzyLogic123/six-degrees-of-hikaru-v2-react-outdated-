eea9a7f363dc618cc78386471e83ad611f5229a0
FuzzyLogic123 <pat.e1@icloud.com>
2022-07-26 14:12:31 +1000

    modified to use inline styles for dynamic colours

--- src/components/HeroHeader/HeroHeader.js
+++ src/components/HeroHeader/HeroHeader.js
@@ -4,7 +4,7 @@ function HeroHeader( { colour, mainText, secondaryText, svg}) {
     return ( 
         <div className="m-8 md:m-20">
             {svg}
-            <h3 className={`text-[${colour}] m-1 text-xl md:text-2xl xl:text-3xl font-bold md:font-medium`}>{secondaryText}</h3>
+            <h3 style={{color: colour}} className='m-1 text-xl md:text-2xl xl:text-3xl font-bold md:font-medium'>{secondaryText}</h3>
             <h1 className="capitalize text-white text-5xl md:text-7xl xl:text-9xl font-bold">{mainText}</h1>
         </div>
      );
