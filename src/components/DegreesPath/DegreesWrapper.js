a892df1020e616309fa9446d0a4bdf78f427f597
Patrick Edwards <pat.e1@icloud.com>
2022-07-30 16:24:54 +1000

    prevent password icon on safari

--- src/components/DegreesPath/DegreesWrapper.js
+++ src/components/DegreesPath/DegreesWrapper.js
@@ -131,7 +131,7 @@ function DegreesWrapper() {
         <div id='six-degrees'>
             <HeroHeader svg={<ConnectionsSvg />} colour={"#818CF8"} secondaryText={"See how you compare"} mainText={"Find your path"} />
             <div className="pt-12 pb-16 flex justify-center gap-6 w-full">
-                <input spellCheck="false" ref={usernameTextRef} className="basis-2/4 inline-block text-white p-3 rounded-md border-2 border-slate-800 bg-slate-900 xl:text-xl text-lg" type="text" placeholder="chess.com username" />
+                <input name="search" spellCheck="false" ref={usernameTextRef} className="basis-2/4 inline-block text-white p-3 rounded-md border-2 border-slate-800 bg-slate-900 xl:text-xl text-lg" type="text" placeholder="chess.com username" />
                 <button onClick={onClickHandler} className='inline-block bg-slate-900 border-slate-800 border-2 p-3 rounded-md xl:text-xl text-lg text-white hover:stroke-slate-50 stroke-slate-400'>
                     <KingSvg />
                 </button>
